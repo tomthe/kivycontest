56e9d42214071d3b816c4c8f08e0d6bc242ae5bb
thomthe <tomi2@web.de>
2014-05-15 17:15:43 +0200

    functional, shows text-answers, has stealth-mode... todo: * polish GUI

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ file to see what will be the root widget.
 import kivy
 from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, StringProperty
 from kivy.app import App
 
 kivy.require('1.0.7')
@@ -17,14 +17,18 @@ kivy.require('1.0.7')
 
 class ColorView(ModalView):
 
-    bcolour = ListProperty([0.5,0.5,0.5,0.5])
+    bcolour = ListProperty([0.5,0.5,0.5,1])
+    answer_text = StringProperty('')
 
 class MainView(BoxLayout):
 
-    def showcolor(self,inputColor):
-        myColorView = ColorView(bcolour=inputColor)
+    def show_answer(self,inputColor,answer_text='',stealth=False):
+        myColorView = ColorView(bcolour=inputColor, answer_text=answer_text)
         myColorView.open()
-
+        if stealth==True:
+            #hide the answer underneath a grey colorView-popup
+            myColorView = ColorView(bcolour=[0.6,0.6,0.6,1], answer_text="tap to show the answer")
+            myColorView.open()
 
 class CrFeedbackApp(App):
 
