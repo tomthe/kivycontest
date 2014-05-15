0158b428b353285af856bcaad9554b1d7f737f9c
thomthe <tomi2@web.de>
2014-05-15 18:15:11 +0200

    added Readme and android.txt; some GUI-polishing

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Classroom-Feedback
+======================
+
+Use smartphone-screens to get immediate feedback on how well the students understand the topic.
+Every student can hold up its smartphone to signal its comprehension of the material:
+* Green: Everything is understood
+* Yellow: Almost there
+* Red: I'm puzzled
+
+Let several people answer multiple-choice-questions at the same time. Hide the answer until everyone choosed one answer.
+
+
+-----------------------------
+
+Requirements:
+--------------
+
+Tested with Kivy 1.8, doesn't have any other dependencies
+should work with earlier versions of Kivy.
--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-'''
-Application from a .kv
-======================
-
-The root application is created from the corresponding .kv. Check the test.kv
-file to see what will be the root widget.
-'''
 
 import kivy
 from kivy.uix.modalview import ModalView
@@ -16,18 +9,19 @@ kivy.require('1.0.7')
 
 
 class ColorView(ModalView):
-
     bcolour = ListProperty([0.5,0.5,0.5,1])
     answer_text = StringProperty('')
-
+    font_size = StringProperty('50dp')
+    
+    
 class MainView(BoxLayout):
 
-    def show_answer(self,inputColor,answer_text='',stealth=False):
-        myColorView = ColorView(bcolour=inputColor, answer_text=answer_text)
+    def show_answer(self,inputColor,answer_text='',stealth=False,font_size='55dp'):
+        myColorView = ColorView(bcolour=inputColor, answer_text=answer_text,font_size=font_size)
         myColorView.open()
         if stealth==True:
             #hide the answer underneath a grey colorView-popup
-            myColorView = ColorView(bcolour=[0.6,0.6,0.6,1], answer_text="tap to show the answer")
+            myColorView = ColorView(bcolour=[0.6,0.6,0.6,1], answer_text='tap to show the answer',font_size='30dp')
             myColorView.open()
 
 class CrFeedbackApp(App):
